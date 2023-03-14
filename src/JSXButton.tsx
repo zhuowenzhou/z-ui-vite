59d5bda2ccf90ad5d6e1fef4f2292c8c77331d74
zhuowenzhou <zhuowenzhou@aliyun.com>
2023-03-14 17:43:58 +0800

    chapter02

---
+++ src/JSXButton.tsx
@@ -0,0 +1,10 @@
+import { defineComponent, h } from "vue";
+
+export default defineComponent({
+
+    name: "JSXButton",
+    render() {
+        return <button>JSX Button 123</button>;
+    },
+
+});
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+const rollupOptions = {
+
+    external: ["vue", "vue-router"],
+    output: {
+        globals: {
+            vue: "Vue",
+        },
+    },
+};
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        vueJsx({})],
+    // 添加库模式配置
+    build: {
+        rollupOptions,
+        minify: false,
+        lib: {
+            entry: "./src/entry.ts",
+            name: "SmartyUI",
+            fileName: "smarty-ui",
+            // 导出模块格式
+            formats: ["es", "umd", "iife"],
+        },
+    },
+});
