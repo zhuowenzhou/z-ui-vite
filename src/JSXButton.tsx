0dcc6e7c5b9e7f6950e042cf3409683a62eae5db
zhuowenzhou <zhuowenzhou@aliyun.com>
2023-03-15 21:49:25 +0800

    接入eslint，prettier

--- src/JSXButton.tsx
+++ src/JSXButton.tsx
@@ -1,10 +1,8 @@
-import { defineComponent, h } from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-
-    name: "JSXButton",
-    render() {
-        return <button>JSX Button 123</button>;
-    },
-
+  name: "JSXButton",
+  render() {
+    return <button>JSX Button 123</button>;
+  },
 });
--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -3,71 +3,71 @@ import "uno.css";
 
 export type ISize = "small" | "medium" | "large";
 export type IColor =
-    | "black"
-    | "gray"
-    | "red"
-    | "yellow"
-    | "green"
-    | "blue"
-    | "indigo"
-    | "purple"
-    | "pink";
+  | "black"
+  | "gray"
+  | "red"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "indigo"
+  | "purple"
+  | "pink";
 
 export const props = {
-    // 新增
-    size: {
-        type: String as PropType<ISize>,
-        default: "medium",
-    },
+  // 新增
+  size: {
+    type: String as PropType<ISize>,
+    default: "medium",
+  },
 
-    color: {
-        type: String as PropType<IColor>,
-        default: "blue",
-    },
+  color: {
+    type: String as PropType<IColor>,
+    default: "blue",
+  },
 
-    round: {
-        type: Boolean,
-        default: false,
-    },
+  round: {
+    type: Boolean,
+    default: false,
+  },
 
-    plain: {
-        type: Boolean,
-        default: false,
-    },
+  plain: {
+    type: Boolean,
+    default: false,
+  },
 
-    icon: {
-        type: String,
-        default: "",
-    },
+  icon: {
+    type: String,
+    default: "",
+  },
 } as const;
 
 export default defineComponent({
-    name: "SButton",
-    props,
-    setup(props, { slots }) {
-        // console.log(`html`, document.querySelector(`#app`)?.innerHTML);
+  name: "SButton",
+  props,
+  setup(props, { slots }) {
+    // console.log(`html`, document.querySelector(`#app`)?.innerHTML);
 
-        const size = {
-            small: {
-                x: "2",
-                y: "1",
-                text: "sm",
-            },
-            medium: {
-                x: "3",
-                y: "1.5",
-                text: "base",
-            },
-            large: {
-                x: "4",
-                y: "2",
-                text: "lg",
-            },
-        };
+    const size = {
+      small: {
+        x: "2",
+        y: "1",
+        text: "sm",
+      },
+      medium: {
+        x: "3",
+        y: "1.5",
+        text: "base",
+      },
+      large: {
+        x: "4",
+        y: "2",
+        text: "lg",
+      },
+    };
 
-        return () => (
-            <button
-                class={`
+    return () => (
+      <button
+        class={`
           py-${size[props.size].y}
           px-${size[props.size].x}
           ${props.round ? "rounded-full" : "rounded-lg"}
@@ -82,14 +82,14 @@ export default defineComponent({
           transition duration-300 ease-in-out transform hover:scale-105
           mx-1
           `}
-            >
-                {props.icon !== "" ? (
-                    <i class={`i-ic-baseline-${props.icon} p-3`}></i>
-                ) : (
-                    ""
-                )}
-                {slots.default ? slots.default() : ""}
-            </button>
-        );
-    },
+      >
+        {props.icon !== "" ? (
+          <i class={`i-ic-baseline-${props.icon} p-3`}></i>
+        ) : (
+          ""
+        )}
+        {slots.default ? slots.default() : ""}
+      </button>
+    );
+  },
 });
