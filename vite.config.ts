5b4e8a36f93fd3299023fee8783df45ee88dffbe
zhuowenzhou <zhuowenzhou@aliyun.com>
2023-03-15 19:56:52 +0800

    接入vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -35,4 +36,15 @@ export default defineConfig({
         },
         cssCodeSplit: true
     },
+    test: {
+        // enable jest-like global test APIs
+        globals: true,
+        // simulate DOM with happy-dom
+        // (requires installing happy-dom as a peer dependency)
+        environment: 'happy-dom',
+        // 支持tsx组件，很关键
+        transformMode: {
+            web: [/.[tj]sx$/]
+        }
+    }
 });
