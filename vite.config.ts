29eea367b96d8c605b7535789c1c242c89c39b77
zhuowenzhou <zhuowenzhou@aliyun.com>
2023-03-16 17:36:11 +0800

    feat: 增加多模块输出和IFFE测试页面

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import Unocss from "./config/unocss";
 
 // https://vitejs.dev/config/
 const rollupOptions = {
-
     external: ["vue", "vue-router"],
     output: {
         globals: {
             vue: "Vue",
         },
         assetFileNames: `assets/[name].css`,
-
     },
 };
 
@@ -22,29 +20,32 @@ export default defineConfig({
         vue(),
         vueJsx({}),
         // 添加UnoCSS插件
-        Unocss()],
+        Unocss(),
+    ],
     // 添加库模式配置
     build: {
         rollupOptions,
-        minify: false,
+        minify: "terser", // boolean | 'terser' | 'esbuild'
+        sourcemap: true, // 输出单独 source文件
+        reportCompressedSize: true, // 生成压缩大小报告
+        cssCodeSplit: true,
         lib: {
             entry: "./src/entry.ts",
             name: "SmartyUI",
             fileName: "smarty-ui",
             // 导出模块格式
-            formats: ["es", "umd", "iife"],
+            formats: ["esm", "umd", "iife"],
         },
-        cssCodeSplit: true
     },
     test: {
         // enable jest-like global test APIs
         globals: true,
         // simulate DOM with happy-dom
         // (requires installing happy-dom as a peer dependency)
-        environment: 'happy-dom',
+        environment: "happy-dom",
         // 支持tsx组件，很关键
         transformMode: {
-            web: [/.[tj]sx$/]
-        }
-    }
+            web: [/.[tj]sx$/],
+        },
+    },
 });
